edb13e08145296b0332894a9410ca7a66baf2ade
Antonio-xavier2005 <cauanunes0501@hotmail.com>
2023-10-11 13:59:12 -0300

    11/10/2023

---
+++ volumepirâmide.java
@@ -0,0 +1,9 @@
+public class volumepirâmide {
+    public static void main(String[] args) {
+        int h = 8;
+        int área = 36;
+        double volume = área * h / 3;
+
+        System.out.println("O volume da pirâmide é: " + volume);
+    }
+}
