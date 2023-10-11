0e05bc353b296d722229c8ab12e21ca46a9c6ad1
Antonio-xavier2005 <cauanunes0501@hotmail.com>
2023-10-11 14:03:11 -0300

    11/10/2023

---
+++ JavaMatemática/volumecilindro.java
@@ -0,0 +1,9 @@
+package JavaMatemática;
+public class volumecilindro {
+    public static void main(String[] args) {
+        int r = 4;
+        int h = 10;
+        double volume = 3.14 * r * r * h;
+        System.out.println("O volume do cilindro é: " + volume);
+    }
+}
--- JavaMatemática/volumepirâmide.java
+++ JavaMatemática/volumepirâmide.java
@@ -1,3 +1,4 @@
+package JavaMatemática;
 public class volumepirâmide {
     public static void main(String[] args) {
         int h = 8;
